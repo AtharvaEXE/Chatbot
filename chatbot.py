5b2a7f3ed1255cdcef715c375587a2b8fbae817f
LOL exe <83347554+AtharvaEXE@users.noreply.github.com>
2022-11-16 18:40:51 +0530

    Dev (#3)
    * str math removed
    
    * added math

--- chatbot.py
+++ chatbot.py
@@ -3,8 +3,9 @@ import wikipedia
 import speech_recognition as sr
 from geopy.geocoders import Nominatim
 import json
+import strmath
 
-name = "IPS Chat Bot"
+name = 'IPS Chat Bot'
 
 engine = pyttsx3.init('sapi5')
 
@@ -24,20 +25,32 @@ def getAnswer(query : str, questions):
     try:
         answer = questions[query]
     except:
-        if "what is " in query:
-            query = query.replace("what is ", "")
+        if 'add' in query or 'plus' in query or '+' in query:
+            answer = 'the answer is ' + strmath.add(query)
+
+        elif 'subtract' in query or 'minus' in query or '-' in query:
+            answer = 'the answer is ' + strmath.subtract(query)
+
+        elif 'divide' in query or 'divided' in query:
+            answer = 'the answer is ' + strmath.divide(query)
+
+        elif 'multiply' in query or 'multiplied' in query or 'times' in query or 'into' in query:
+            answer = 'the answer is ' + strmath.multiply(query)
+
+        elif 'what is ' in query:
+            query = query.replace('what is ', '')
             answer = searchWiki(query)
     
-        elif "who is " in query:
-            query = query.replace("who is ", "")
+        elif 'who is ' in query:
+            query = query.replace('who is ', '')
             answer = searchWiki(query)
 
-        elif "where is " in query:
-            query = query.replace("where is ", "")
-            answer = query + " is located at " + str(locate(query))
+        elif 'where is ' in query:
+            query = query.replace('where is ', '')
+            answer = query + ' is located at ' + str(locate(query))
 
         else:
-            answer = "i do not know about that"
+            answer = 'i do not know about that'
 
     return answer
 
@@ -47,38 +60,37 @@ def takeCommand():
     with sr.Microphone() as source:       
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
-        print("listening")
+        print('listening')
         audio = r.listen(source)
 
     try:
-        print("recognizing")
+        print('recognizing')
         query = (r.recognize_google(audio, language ='en-in'))      
     except:
-        query = "Please repeat it"
+        query = 'Please repeat it'
         return query
 
     query.lower()
     print(query)
     return query
 
-def searchWiki(query):
+def searchWiki(query : str):
     try:
         search = wikipedia.search(query)
         result = wikipedia.summary(search[0], 2)
     except:       
-        result = "sorry, i can not find any results realating to " + query + " on wikipedia"
+        result = 'sorry, i can not find any results realating to ' + query + ' on wikipedia'
 
     print(result)
     return result
 
 def locate(place):
-    geolocator = Nominatim(user_agent = "IPS Chat Bot")
-    return geolocator.geocode("Taj Mahal", language = "en")
+    geolocator = Nominatim(user_agent = 'IPS Chat Bot')
+    return geolocator.geocode('Taj Mahal', language = 'en')
 
 def Greet(engine, name):
-    engine.say("Welcome to IPS")
-    engine.say("My name is " + name)
-    engine.say("How may I help you")
+    engine.say('My name is ' + name)
+    engine.say('How may I help you')
     engine.runAndWait()
 
 Greet(engine, name)
